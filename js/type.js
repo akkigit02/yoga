47c90c86fdbcf13fd46c332bf546579e4ae9c74a
akkigit02 <akkiuiux@gmail.com>
2026-06-12 18:18:50 +0530

    Add files via upload

---
+++ js/type.js
@@ -0,0 +1,11 @@
+window.ityped.init(document.querySelector('.ityped'),{
+    // strings: ['Body & Soul'],
+    strings: [
+        'Body & Soul',
+        'Inner Peace',
+        'Healthy Life',
+    ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
